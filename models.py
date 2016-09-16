3ef34e4c2f77f7de8fd0ab25019a1f945492c1d8
Felix Morgner <felix.morgner@gmail.com>
2016-09-17 01:52:25 +0200

    Add models

--- models.py
+++ models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+
+class Event(models.Model):
+    name     = models.CharField(max_length=128)
+    tag      = models.ManyToManyField(Tag)
+    location = models.CharField(max_length=128)
+
+class Occurrence(models.Model):
+    weekday  = models.IntegerField()
+    start    = models.TimeField()
+    duration = models.IntegerField()
+    event    = models.ForeignKey(Event)
+
+class SpecialOccurrence(models.Model):
+    date      = models.DateField()
+    occurence = models.ForeignKey(Occurrence)
+
+class Plan(models.Model):
+    occurence = models.ForeignKey(Occurrence, null=True)
+
+class Slot(models.Model):
+    plan  = models.OneToOneField(Plan, null=True)
+    start = models.DateField()
+
